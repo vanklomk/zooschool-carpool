9262b3662a2ab2c4abfb30979b77dc14e3a373d6
vanklomk <vanklomk@gmail.com>
2025-08-14 22:13:03 -0400

    New push

--- app/api/trips/route.ts
+++ app/api/trips/route.ts
@@ -54,6 +54,6 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json({ trip: data }, { status: 201 });
   } catch (e) {
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    return NextResponse.json({ error: "Internal server error, tell Keith" }, { status: 500 });
   }
 }
